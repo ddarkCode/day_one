aac4bc05ccf5079854143fb53fcfe97ec03bb490
Nnamani Ugochukwu Frederick <royal.ugoh@gmail.com>
2022-08-15 14:19:09 +0100

    Complete Day One Project

--- day_one/day_one.py
+++ day_one/day_one.py
@@ -52,3 +52,20 @@ print('b: ', b)
 
 #Write your code above this line 👆
 ####################################
+
+#Day One Project
+
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
+print('Good day and welcome to the band name generator app')
+city_name = input('Which city did you grow up in?\n')
+pet_name = input('What is the name of your pet?\n')
+print('Your Band Name Is: ', city_name + ' ' + pet_name)
